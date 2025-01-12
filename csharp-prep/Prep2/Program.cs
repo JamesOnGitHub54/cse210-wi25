44bc03afb6ae10a3dcf528b7c3604147b96ef350
JamesOnGitHub54 <jamesg521@outlook.com>
2025-01-11 17:02:31 -0700

    Completed Prep2 Core

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?: ");
+        int user_gp = int.Parse(Console.ReadLine());
+
+        string letter = "";
+        string message = "";
+        
+        //Convert grade percentage to letter grade
+        if (user_gp >= 90){
+            letter = "A";
+        }
+        else if (user_gp >= 80){
+            letter = "B";
+        }
+        else if (user_gp >= 70){
+            letter = "C";
+        }
+        else if (user_gp >= 60){
+            letter = "D";
+        }
+        else letter = "F";
+
+        //Determine passing or failing with message
+        if (user_gp >= 70){
+            message = "Congratulations! You have passed the course!";
+
+        }
+        else message = "Unfortunately, you did not pass. You can do it, next time!";
+
+        Console.WriteLine($"You have earned: {letter}. {message}");
+
     }
 }
