139ba1723783ef1748f12ea8adc2946b64e5fa75
JamesOnGitHub54 <jamesg521@outlook.com>
2025-02-13 03:07:02 -0700

    Delevop03 Progress1

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,8 @@
+public class Reference{
+    private string _book;
+    private string _chapterStart;
+    private string _chapterEnd;
+    private string _verseStart;
+    private string _verseEnd;
+
+}
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,35 @@
+public class Scripture
+{
+    public List<string> _scriptures = new List<string>(){
+        "And now, O my son Helaman, behold, thou art in thy youth, and therefore, I beseech of thee that thou wilt hear my words and learn of me; for I do know that whosoever shall put their trust in God shall be supported in their trials, and their troubles, and their afflictions, and shall be lifted up at the last day.",
+        "Peace I leave with you, my peace I give unto you: not as the world giveth, give I unto you. Let not your heart be troubled, neither let it be afraid.",
+        ""
+    };
+
+    public List<string> _names = new List<string>(){
+        "Alma",
+        "John",
+        ""
+    };
+
+    public List<string> _startChapter = new List<string>(){
+        "36",
+        "14",
+        ""
+    };
+
+    public List<string> _startVerse = new List<string>(){
+        "3",
+        "27",
+        ""
+    };
+
+    public List<string> _
+
+    public string GetScripture()
+    {
+        return "Scripture";
+    }
+}
+
+///https://www.churchofjesuschrist.org/comeuntochrist/believe/bible/20-scriptures-about-hope
